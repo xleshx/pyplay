94414176e9f971b187537f63964cdb4787aa4cb6
Alexey Antipov <alexey.lesh@gmail.com>
2018-09-19 09:30:29 +0200

    Change project structure, add punctuation agnostic support

--- worddistcount/test/test.py
+++ worddistcount/test/test.py
@@ -5,10 +5,12 @@ from worddistcount import WordDistanceCounter, calculate_distance
 class TestStringMethods(unittest.TestCase):
 
     def test_integration(self):
+        """Basic integration test case with I/O op coverage"""
         word_counter = WordDistanceCounter("test_file.txt")
         self.assertEqual(2, word_counter.find_shortest_distance("motivation", "development"))
 
     def test_duplicates(self):
+        """Unit test with duplicates"""
         text_with_duplicates = "ops ops dev dev qa ops"
         self.assertEqual(1, calculate_distance(text_with_duplicates, "dev", "ops"))
 
@@ -17,7 +19,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(1, calculate_distance(text_with_duplicates, "dev", "ops"))
 
     def test_case_insensitive(self):
-        text_with_duplicates = "ops ops dev dev qa ops dev"
+        text_with_duplicates = "ops Ops dev deV qa ops dev"
+        self.assertEqual(1, calculate_distance(text_with_duplicates, "Dev", "opS"))
+
+    def test_punctuation_agnostic(self):
+        text_with_duplicates = "ops. ops! !!dev dev. qa<<< ops!:( dev=+>"
         self.assertEqual(1, calculate_distance(text_with_duplicates, "Dev", "opS"))
 
 
--- worddistcount/worddistcount/__init__.py
+++ worddistcount/worddistcount/__init__.py
@@ -1,8 +1,11 @@
 import argparse
+import string
 
 
-def calculate_distance(text, source, target):
-    words, source, target = make_lower_case(text, source, target)
+def calculate_distance(text: str, source: str, target: str):
+    """Calculates a distance between source and target within text."""
+
+    words, source, target = prepare_input_strings(text, source, target)
 
     start_idx_candidate = None
     for i, word in enumerate(words):
@@ -13,20 +16,35 @@ def calculate_distance(text, source, target):
             return end_idx_candidate - start_idx_candidate - 1
 
 
-def make_lower_case(text, source, target):
-    words = [word.lower() for word in text.split()]
-    return words, source.lower(), target.lower()
+def prepare_input_strings(text: str, source: str, target: str) -> {str, str, str}:
+    """Helper function for input preparation."""
+
+    words = [trim_and_lower(word) for word in text.split()]
+    return words, trim_and_lower(source), trim_and_lower(target)
+
+
+def trim_and_lower(source: str) -> str:
+    """Trims punctuation and make letters lower case."""
+
+    return source.translate(str.maketrans('', '', string.punctuation)).lower()
 
 
 class WordDistanceCounter:
-    def __init__(self, filename):
+    """Class expect filename in constructor but don't do any IO before call find_shortest_distance function."""
+
+    def __init__(self, filename: str):
         self.filename = filename
 
     def load_txt_from_file(self):
         with open(self.filename) as txtFile:
             return txtFile.read()
 
-    def find_shortest_distance(self, source, target):
+    def find_shortest_distance(self, source: str, target: str):
+        """Main find distance method.
+
+         Loads the file content each time being invoked and applies calculate_distance() function on it.
+
+         """
         text = self.load_txt_from_file()
         return calculate_distance(text, source, target)
 
